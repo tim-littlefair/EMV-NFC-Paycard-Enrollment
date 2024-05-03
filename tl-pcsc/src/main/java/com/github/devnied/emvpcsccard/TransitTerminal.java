98579ea2182345d56c5ad5eaffc1b4475af0a45a
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2024-05-03 10:24:08 +0800

    Terminal now returns value based on EMV tag for unexpected requests

--- tl-pcsc/src/main/java/com/github/devnied/emvpcsccard/TransitTerminal.java
+++ tl-pcsc/src/main/java/com/github/devnied/emvpcsccard/TransitTerminal.java
@@ -2,6 +2,7 @@ package com.github.devnied.emvpcsccard;
 
 import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.apache.commons.lang3.StringUtils;
@@ -32,7 +33,9 @@ public class TransitTerminal implements ITerminal {
     private byte[] m_additionalTerminalCapabilities;
     private byte m_terminalType;
     private byte[] m_amountAuthorizedBCD;
+    private byte[] m_amountOtherBCD;
     private byte[] m_unpredictableNumber;
+    private byte[] m_terminalVerificationResults;
 
     public TransitTerminal() {
         m_countryCode = CountryCodeEnum.AU;
@@ -52,9 +55,13 @@ public class TransitTerminal implements ITerminal {
         m_terminalType = (byte) 0x25;
 
         m_amountAuthorizedBCD = BytesUtils.fromString("000000000000");
+        m_amountOtherBCD = BytesUtils.fromString("000000000000");
         
         m_unpredictableNumber = new byte[4];
         random.nextBytes(m_unpredictableNumber);
+
+        // Terminal verification results
+        m_terminalVerificationResults = BytesUtils.fromString("000000000000");
     }
 
     private void setArrayBit(byte[] array, int byteIndex, int bitIndex, boolean bitValue) {
@@ -78,79 +85,73 @@ public class TransitTerminal implements ITerminal {
         byte val[] = null;
         if (tag == EmvTags.TERMINAL_TRANSACTION_QUALIFIERS) {
             val = new byte[4];
-
-            // references:
-            // https://paymentcardtools.com/emv-tag-decoders/ttq
-            // EMV: 
-            // Visa: EMV Book C.3 v2.11 p113-114
-            // TODO: determine whether this needs to be different between Visa 
-            // and other brands
-
-            setArrayBit(val,1, 8, false); // MSD not supported
-            // ret[0] bit 7 RFU = 0 
-            setArrayBit(val,1, 6, true);  // Visa: qVSDC supported
-            setArrayBit(val,1, 5, false); // Contact not supported
-            // Transit terminals are do all taps offline, but might pretend to 
-            // be online-capable so that the card generates an ARQC for 
-            // deferred authorisation at the payment gateway.
-            setArrayBit(val,1, 4, false); // not offline only
-            setArrayBit(val,1, 3, false); // Online PIN not supported
-            setArrayBit(val,1, 2, false); // Signature not supported
-            setArrayBit(val,1, 1, true);  // ODA for online supported
-
-            setArrayBit(val,2, 8, true);  // Online cryptogram required
-            setArrayBit(val,2, 7, false); // CVM not required by terminal
-            setArrayBit(val,2, 6, false); // Offline PIN not supported
-            // byte 2 bits 5-1 RFU = 0 for Visa
-            // TODO: Work out whether this is OK for other brands
-
-            setArrayBit(val,3, 8, false);  // Issuer updates not supported
-            // Turn on CDCVM so that we can see whether it is triggered
-            setArrayBit(val,3, 7, true);   // Consumer device CDM supported
-
-            // byte 3 bits 6-1 RFU = 0 for Visa
-
-            // TODO: Work out whether this is OK for other brands
-
-            // byte 4 all bits RFU = 0
-        } else {
-            if (tag == EmvTags.TERMINAL_COUNTRY_CODE) {
-                val = BytesUtils.fromString(StringUtils.leftPad(String.valueOf(
-                    m_countryCode.getNumeric()), 
-                    pTagAndLength.getLength() * 2,"0"
-                ));
-            } else if (pTagAndLength.getTag() == EmvTags.TRANSACTION_CURRENCY_CODE) {
-                val = BytesUtils.fromString(StringUtils.leftPad(
-                    String.valueOf(CurrencyEnum.find(m_countryCode, m_currencyCode).getISOCodeNumeric()),
-                    pTagAndLength.getLength() * 2, "0"
-                ));
-            } else if (pTagAndLength.getTag() == EmvTags.TRANSACTION_DATE) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
-                val = BytesUtils.fromString(sdf.format(new Date()));
-            } else if (
-                pTagAndLength.getTag() == EmvTags.TRANSACTION_TYPE || 
-                pTagAndLength.getTag() == EmvTags.TERMINAL_TRANSACTION_TYPE
-            ) {
-                val = new byte[] { (byte) TransactionTypeEnum.PURCHASE.getKey() };
-            } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_TYPE) {
-                val = new byte[] { m_terminalType };
-            } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_CAPABILITIES) {
-                val = m_terminalCapabilities;
-            } else if (pTagAndLength.getTag() == EmvTags.ADDITIONAL_TERMINAL_CAPABILITIES) {
-                val = m_additionalTerminalCapabilities;
-            } else if (pTagAndLength.getTag() == EmvTags.UNPREDICTABLE_NUMBER) {
-                val = m_unpredictableNumber;
-            } else if (pTagAndLength.getTag() == EmvTags.AMOUNT_AUTHORISED_NUMERIC) {
-                val = m_amountAuthorizedBCD;
-/*                 
-            } else if (pTagAndLength.getTag() == EmvTags.DS_REQUESTED_OPERATOR_ID) {
-                val = BytesUtils.fromString("7A45123EE59C7F40");
-            } else if (pTagAndLength.getTag() == EmvTags.MERCHANT_TYPE_INDICATOR) {
-                val = new byte[] { 0x01 };
-            } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_TRANSACTION_INFORMATION) {
-                val = new byte[] { (byte) 0xC0, (byte) 0x80, 0 };
+            populateDefaultTTQ(val);
+        } else if (tag == EmvTags.TERMINAL_COUNTRY_CODE) {
+            val = BytesUtils.fromString(StringUtils.leftPad(String.valueOf(
+                m_countryCode.getNumeric()), 
+                pTagAndLength.getLength() * 2,"0"
+            ));
+        } else if (pTagAndLength.getTag() == EmvTags.TRANSACTION_CURRENCY_CODE) {
+            val = BytesUtils.fromString(StringUtils.leftPad(
+                String.valueOf(CurrencyEnum.find(m_countryCode, m_currencyCode).getISOCodeNumeric()),
+                pTagAndLength.getLength() * 2, "0"
+            ));
+        } else if (pTagAndLength.getTag() == EmvTags.TRANSACTION_DATE) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+            val = BytesUtils.fromString(sdf.format(new Date()));
+        } else if (
+            pTagAndLength.getTag() == EmvTags.TRANSACTION_TYPE || 
+            pTagAndLength.getTag() == EmvTags.TERMINAL_TRANSACTION_TYPE
+        ) {
+            val = new byte[] { (byte) TransactionTypeEnum.PURCHASE.getKey() };
+        } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_TYPE) {
+            val = new byte[] { m_terminalType };
+        } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_CAPABILITIES) {
+            val = m_terminalCapabilities;
+        } else if (pTagAndLength.getTag() == EmvTags.ADDITIONAL_TERMINAL_CAPABILITIES) {
+            val = m_additionalTerminalCapabilities;
+        } else if (pTagAndLength.getTag() == EmvTags.UNPREDICTABLE_NUMBER) {
+            val = m_unpredictableNumber;                
+        } else if (pTagAndLength.getTag() == EmvTags.AMOUNT_AUTHORISED_NUMERIC) {
+            val = m_amountAuthorizedBCD;
+        } else if (pTagAndLength.getTag() == EmvTags.AMOUNT_OTHER_NUMERIC) {
+            val = m_amountOtherBCD;
+        } else if (pTagAndLength.getTag() == EmvTags.AMOUNT_OTHER_NUMERIC) {
+            val = m_amountOtherBCD;
+        } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_VERIFICATION_RESULTS) {
+            val = m_terminalVerificationResults;
+/*
+        } else if (pTagAndLength.getTag() == EmvTags.DS_REQUESTED_OPERATOR_ID) {
+            val = BytesUtils.fromString("7A45123EE59C7F40");
+        } else if (pTagAndLength.getTag() == EmvTags.MERCHANT_TYPE_INDICATOR) {
+            val = new byte[] { 0x01 };
+        } else if (pTagAndLength.getTag() == EmvTags.TERMINAL_TRANSACTION_INFORMATION) {
+            val = new byte[] { (byte) 0xC0, (byte) 0x80, 0 };
 */
+        } else {
+            // value will be equal to the hex bytes of the tag, 
+            // left-padded with zeros or truncated to 
+            // the required length
+            int valueLength = pTagAndLength.getLength();
+            val = new byte[valueLength];
+            int tagLength = tag.getTagBytes().length;
+            if(tagLength>=valueLength) {
+                // truncated (if necessary)
+                val = Arrays.copyOfRange(tag.getTagBytes(),0,valueLength);
+            } else {
+                // left padded
+                System.arraycopy(
+                    tag.getTagBytes(),0,
+                    val, valueLength-tagLength,
+                    tagLength
+                );
             }
+            LOGGER.warn(String.format(
+                "Unexpected tag %s(%s) requested from TransitTerminal, returning '%s'",
+                tag.getName(), 
+                BytesUtils.bytesToStringNoSpace(tag.getTagBytes()),
+                BytesUtils.bytesToString(val)
+            ));
         }
         if (val != null) {
             System.arraycopy(val, 0, ret, Math.max(ret.length - val.length, 0), Math.min(val.length, ret.length));
@@ -159,5 +160,42 @@ public class TransitTerminal implements ITerminal {
             pTagAndLength.toString() + ": " + BytesUtils.bytesToString(ret)
         );
         return ret;
+    }
+
+    private void populateDefaultTTQ(byte[] val) {
+        // references:
+        // https://paymentcardtools.com/emv-tag-decoders/ttq
+        // EMV: 
+        // Visa: EMV Book C.3 v2.11 p113-114
+        // TODO: determine whether this needs to be different between Visa 
+        // and other brands
+
+        setArrayBit(val,1, 8, false); // MSD not supported
+        // ret[0] bit 7 RFU = 0 
+        setArrayBit(val,1, 6, true);  // Visa: qVSDC supported
+        setArrayBit(val,1, 5, false); // Contact not supported
+        // Transit terminals are do all taps offline, but might pretend to 
+        // be online-capable so that the card generates an ARQC for 
+        // deferred authorisation at the payment gateway.
+        setArrayBit(val,1, 4, false); // not offline only
+        setArrayBit(val,1, 3, false); // Online PIN not supported
+        setArrayBit(val,1, 2, false); // Signature not supported
+        setArrayBit(val,1, 1, true);  // ODA for online supported
+
+        setArrayBit(val,2, 8, true);  // Online cryptogram required
+        setArrayBit(val,2, 7, false); // CVM not required by terminal
+        setArrayBit(val,2, 6, false); // Offline PIN not supported
+        // byte 2 bits 5-1 RFU = 0 for Visa
+        // TODO: Work out whether this is OK for other brands
+
+        setArrayBit(val,3, 8, false);  // Issuer updates not supported
+        // Turn on CDCVM so that we can see whether it is triggered
+        setArrayBit(val,3, 7, true);   // Consumer device CDM supported
+
+        // byte 3 bits 6-1 RFU = 0 for Visa
+
+        // TODO: Work out whether this is OK for other brands
+
+        // byte 4 all bits RFU = 0
     }
 }
